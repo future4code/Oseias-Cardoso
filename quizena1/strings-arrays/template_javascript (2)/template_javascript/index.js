094692b79203254ae89c8386ab81877f1126084f
OseiasBispoCardoso <okwalu@gmail.com>
2021-09-14 21:42:04 -0300

    Adicionar exercícios leitura de código

---
+++ quizena1/strings-arrays/template_javascript (2)/template_javascript/index.js
@@ -0,0 +1,33 @@
+// 1
+//Indique todas as mensagens impressas no console, SEM EXECUTAR o programa.
+// let array = cachorro
+// console.log('a. ', array) // a.  cachorro
+
+// array = null
+// console.log('b. ', array) // b. null
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length) // c. 11
+
+// let i = 0
+// console.log('d. ', array[i]) //d. 0
+
+// array[i+1] = 19
+// console.log('e. ', array) // e. 19
+
+// const valor = array[i+6]
+// console.log('f. ', valor) // f. array[i+6]
+
+2//
+//Leia o código abaixo com atenção
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length) // DIGITE UMA FRASE /   I  / 16
+
+
+//Exercícios de escrita de códigoFaça um programa que pergunte ao usuário seu nome e seu e-mail. 
+//Em seguida, imprima na tela a seguinte mensagem:
+//O e-mail emailDoUsuario foi cadastrado com sucesso. Seja bem-vinda(o), nomeDoUsuario!
+
+const nomeDoUsuario = prompt("Qual é o seu nome?")
+const emailDoUsuario = prompt("Qual é o seu e-mail")
